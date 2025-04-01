0a48bd7c198147a1367d4cc043c786c7cd24fbbd
Naveenbandari26 <bandarin29@gmail.com>
2025-04-01 16:04:13 +0530

    Update main.cpp

--- main/main.cpp
+++ main/main.cpp
@@ -10,7 +10,6 @@ public:
         if (n < 3) return 0;
 
         int longest = 0;
-
         for (int i = 1; i < n - 1; i++) {
             if (arr[i - 1] < arr[i] && arr[i] > arr[i + 1]) {
                 int left = i - 1;
