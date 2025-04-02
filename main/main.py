42f4ee3b0073ea17b77262c979bd8d979f5d022e
Naveenbandari26 <bandarin29@gmail.com>
2025-04-02 14:52:05 +0530

    removed the main code

--- main/main.py
+++ main/main.py
@@ -1,26 +1,4 @@
 __package__ = 'main'
 class Solution: #class
     def longestMountain(self, arr):
-        n = len(arr)
-        if n < 3:
-            return 0
-
-        longest = 0
-
-        for i in range(1, n - 1):
-            if arr[i - 1] < arr[i] > arr[i + 1]:
-                left = i - 1
-                right = i + 1
-
-                while left > 0 and arr[left - 1] < arr[left]:
-                    left -= 1
-
-                while right < n - 1 and arr[right] > arr[right + 1]:
-                    right += 1
-
-                length = right - left + 1
-                if length >= 3:
-                    longest = max(longest, length)
-
-        return longest
-
+       
