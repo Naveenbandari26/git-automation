ddb2b22667b69950b7732f29ac853082ffa1db47
Naveenbandari26 <bandarin29@gmail.com>
2025-04-02 16:14:04 +0530

    update-2

--- solutions/solution.java
+++ solutions/solution.java
@@ -20,6 +20,5 @@ public class solution {
     }
     public static void main(String[] args) {
         System.out.println("Main class compiled successfully. Run MainTest.java for tests.");
-    
     }
 }
