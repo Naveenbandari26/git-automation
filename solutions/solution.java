619c55e660adf5e0e58afde619e6ff12f603851d
Naveenbandari26 <bandarin29@gmail.com>
2025-04-02 16:10:45 +0530

    update yml, java

--- solutions/solution.java
+++ solutions/solution.java
@@ -20,6 +20,6 @@ public class solution {
     }
     public static void main(String[] args) {
         System.out.println("Main class compiled successfully. Run MainTest.java for tests.");
-        // code here
+    
     }
 }
