aae07f4966dc708051bf270467c5292a575a7bae
Naveenbandari26 <bandarin29@gmail.com>
2025-04-03 21:19:52 +0530

    Update solution.java

--- solutions/solution.java
+++ solutions/solution.java
@@ -1,7 +1,6 @@
 package solutions;
 public class solution {
     public static int longestMountain(int[] arr) {
-        return 0;
     }
     public static void main(String[] args) {
         System.out.println("Main class compiled successfully. Run MainTest.java for tests.");
